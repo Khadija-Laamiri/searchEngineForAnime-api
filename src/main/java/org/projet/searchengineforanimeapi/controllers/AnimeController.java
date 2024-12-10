bae039a29ae73cec4522eae739a0a688ba9dbbf4
sinox2003 <mohaennouass@gmail.com>
2024-12-10 18:46:05 +0100

    connecting with the search engine in python

--- src/main/java/org/projet/searchengineforanimeapi/controllers/AnimeController.java
+++ src/main/java/org/projet/searchengineforanimeapi/controllers/AnimeController.java
@@ -1,11 +1,16 @@
 package org.projet.searchengineforanimeapi.controllers;
 
+import org.projet.searchengineforanimeapi.dtos.AnimeDTO;
 import org.projet.searchengineforanimeapi.services.AnimeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/animes")
 public class AnimeController {
 
     private final AnimeService animeService;
@@ -16,6 +21,7 @@ public class AnimeController {
 
     @GetMapping("/import-anime")
     public String importAnime(@RequestParam String filePath) {
+        System.out.println(filePath);
         try {
             animeService.importAnimeData(filePath);
             return "Anime data imported successfully!";
@@ -24,4 +30,11 @@ public class AnimeController {
             return "Failed to import anime data: " + e.getMessage();
         }
     }
+
+    @GetMapping("/search")
+    public List<AnimeDTO> search(@RequestParam String query) throws Exception {
+        return animeService.search(query);
+    }
+
+
 }
